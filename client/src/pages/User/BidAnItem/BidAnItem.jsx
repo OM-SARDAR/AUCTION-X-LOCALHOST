b40f01bc6b118e5bd7a5b979d333def52ac907b6
Om Sardar <omsardar14@gmail.com>
2025-01-12 22:38:19 +0530

    Changes at BidAnItem page

--- client/src/pages/User/BidAnItem/BidAnItem.jsx
+++ client/src/pages/User/BidAnItem/BidAnItem.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
-import ProductListTwo from '../../../components/ProductList2/ProductListTwo';
-import ProductList from '../../../components/ProductList/ProductList';
-import Header from './SubComponent/BidHeader';
-import SlideDeisgn from '../../../components/SlideDeisgn/SlideDeisgn';
-import './BidAnItem.css';
-
+import React from "react";
+import ProductListTwo from "../../../components/ProductList2/ProductListTwo";
+import ProductList from "../../../components/ProductList/ProductList";
+import Header from "./SubComponent/BidHeader";
+import SlideDeisgn from "../../../components/SlideDeisgn/SlideDeisgn";
+import "./BidAnItem.css";
 
 const BidAnItem = () => {
   return (
-    <div className='BidAnItem'>
+    <div className="BidAnItem">
       <section>
-        <Header/>
-        <SlideDeisgn/>
-        <h2 style={{ textAlign: 'center', marginTop: '70px', marginBottom: '70px' }} className=" neon-border"><span className='SpanTag'>Ongoing Bids</span></h2>
+        <Header />
+        <SlideDeisgn />
+        <h2
+          style={{
+            textAlign: "center",
+            marginTop: "70px",
+            marginBottom: "70px",
+          }}
+          className=" neon-border"
+        >
+          <span className="SpanTag">More items to consider</span>
+        </h2>
         <ProductListTwo />
-        <h2 style={{ textAlign: 'center', marginTop: '110px', marginBottom: '70px' }} className=" neon-border"><span className='SpanTag'>You may like</span></h2>
+        <h2
+          style={{
+            textAlign: "center",
+            marginTop: "110px",
+            marginBottom: "70px",
+          }}
+          className=" neon-border"
+        >
+          <span className="SpanTag">You may like</span>
+        </h2>
         <ProductList />
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default BidAnItem
+export default BidAnItem;
